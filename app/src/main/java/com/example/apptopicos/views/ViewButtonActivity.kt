76c5e0159c3b21df3413f3ddea7f596dbff388e2
EbenCT <cayoeben64@gmail.com>
2024-10-17 03:07:17 -0300

    Reconoce cuando presiona y suelta el boton

--- app/src/main/java/com/example/apptopicos/views/ViewButtonActivity.kt
+++ app/src/main/java/com/example/apptopicos/views/ViewButtonActivity.kt
@@ -5,7 +5,10 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
 import android.view.WindowManager
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.apptopicos.R
 
@@ -21,7 +24,23 @@ class ViewButtonActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         window.setType(WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY)
         setContentView(R.layout.activity_view_button)
+        // Obtener referencia del botón
+        val button = findViewById<Button>(R.id.fullscreen_button)
 
+        // Configurar el listener para detectar cuando se presiona y se suelta el botón
+        button.setOnTouchListener { _, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    // Se presionó el botón
+                    Log.d("MiApp", "Presionaste el botón")
+                }
+                MotionEvent.ACTION_UP -> {
+                    // Se soltó el botón
+                    Log.d("MiApp", "Dejaste de presionar el botón")
+                }
+            }
+            true
+        }
         // Registrar el receptor de broadcast para cerrar el Activity
         registerReceiver(closeReceiver, IntentFilter("com.example.apptopicos.CLOSE_ACTIVITY"))
     }
